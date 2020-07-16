c96faedbb71ba7261c51c5c74f1d255662e3b0dc
Nycolas Silvestre <nycolaszsilvestre@gmail.com>
2020-07-16 19:28:53 +0200

    .

--- com.ual.labprojeto.formcreator/src/main/java/Main.java
+++ com.ual.labprojeto.formcreator/src/main/java/Main.java
@@ -5,11 +5,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main extends Application {
 
